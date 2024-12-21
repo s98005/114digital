8d0563653bdd95fe26530a67380d79c6512ab1d7
s98005 <92238487+s98005@users.noreply.github.com>
2024-12-21 14:33:17 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,57 @@
+const Group = document.getElementById("group");
+const bar = document.getElementById('social-media-bar'); // 獲取 #bar
+const place1 = document.getElementById('place1');
+const place2 = document.getElementById('place2');
+const place1p = place1.querySelector('p');
+const place2p = place2.querySelector('p');
+const placeinfo1 = document.getElementById('place-info1');
+const placeinfo2 = document.getElementById('place-info2');
+const Rectangle = document.getElementById('Rectangle');
+
+var place = 1 ;
+
+Group.addEventListener('click', () => {
+    if (bar.style.display === 'none' || bar.style.display === '') {
+        bar.style.display = 'flex'; // 顯示 #bar
+    } else {
+        bar.style.display = 'none'; // 隱藏 #bar
+    }
+});
+
+place1.addEventListener('mouseover', function() {
+    if (place == 2){
+        place1p.style.color = 'rgba(150, 239, 255, 1)';
+        place1p.style.textShadow = ' 0 0 5px rgba(255, 255, 255, 0.7)';
+    }
+});
+
+place1.addEventListener('mouseout', function() {
+        place1p.style.color = '#FFF';
+        place1p.style.textShadow = 'none';
+});
+
+place2.addEventListener('mouseover', function() {
+    if (place == 1){
+        place2p.style.color = 'rgba(150, 239, 255, 1)';
+        place2p.style.textShadow = ' 0 0 5px rgba(255, 255, 255, 0.7)';
+    }
+});
+
+place2.addEventListener('mouseout', function() {
+    place2p.style.color = '#FFF';
+    place2p.style.textShadow = 'none';
+});
+
+place1.addEventListener('click', () => {
+    placeinfo1.style.display = 'flex'; // 顯示 #bar
+    placeinfo2.style.display = 'none'; // 隱藏 #bar
+    Rectangle.style.top = '78px';
+    place = 1; 
+});
+
+place2.addEventListener('click', () => {
+    placeinfo2.style.display = 'flex'; // 顯示 #bar
+    placeinfo1.style.display = 'none'; // 隱藏 #bar
+    Rectangle.style.top = '360px';
+    place = 2;
+});
